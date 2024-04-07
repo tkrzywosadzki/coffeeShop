337769ae6048eaff0855ec75dc042ea0fea6cfcd
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-04-07 21:46:25 +0200

    fix css and add server.mjs

--- src/js/settings.js
+++ src/js/settings.js
@@ -29,7 +29,7 @@ export const classNames = {
 
 export const settings = {
     db: {
-        url: '//localhost:3131',
+        url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
         home: 'home',
         products: 'products',
         contact: 'contact',
